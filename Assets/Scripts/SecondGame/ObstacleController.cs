f80dd3aed11a296a99bb0557cda984821528989c
silvedko <silvedko@gmail.com>
2014-10-27 15:49:24 +0400

    Add plugins

---
+++ Assets/Scripts/SecondGame/ObstacleController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObstacleController : MonoBehaviour 
+{
+	public GameObject obstaclePrefab;
+
+	private bool isRightPosition = false;
+	List<GameObject> poolObject;
+
+	public void StartMove ()
+	{
+
+	}
+
+	Vector3 SetObstaclePosition ()
+	{
+		Vector3 pos;
+		if(isRightPosition)
+			pos = new Vector3 (SecondGameConstants.MaxObstaclePosition, 0f, 0f);
+		else 
+			pos = new Vector3 (-SecondGameConstants.MaxObstaclePosition, 0f, 0f);
+		isRightPosition = !isRightPosition;
+
+		return pos;
+	}
+
+	void Start () 
+	{
+		poolObject = new List<GameObject> (2);
+
+		GameObject obstacle1 = (GameObject) Instantiate (obstaclePrefab, SetObstaclePosition(), Quaternion.identity);
+//		poolObject.Add
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/Scripts/SecondGame/SecondGameConstants.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecondGameConstants : MonoBehaviour 
+{
+	public static float StepForMainGameObject = 0.065f;
+	public static float MaxObstaclePosition = 3f;
+
+}
